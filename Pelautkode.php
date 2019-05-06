ee8a75fb34a8e0143e3d5a86c62460ae0cde3c4c
pelautkode <50316418+pelautkode@users.noreply.github.com>
2019-05-06 18:03:29 +0700

    Update and rename HMA.php to Pelautkode.php

---
+++ Pelautkode.php
@@ -0,0 +1,24 @@
+<?php
+$green  = "\e[92m";
+$red    = "\e[91m";
+$yellow = "\e[93m";
+$blue   = "\e[36m";
+echo "\n$green
+                        _    
+                     _\( )/_            
+Hi! i'm Pelaut Kode   /(O)\  //  \\        
+                            _\\()//_    
+Trust me, i'm Love You.    / //  \\ \      
+                            | \__/ |\n".$red.
+"======================================";
+echo $blue."
+Author  : Pelaut Kode
+Github  : http://github.com/Pelautkode
+Team    : System Cyber Security
+Date    : 06-05-2019\n";
+echo $red."=====================================".$green."\n";
+sleep(1);
+}
+	}
+
+?>
