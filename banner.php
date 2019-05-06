32e6cf4052437af05bfa16d242b8a0d65d21eb54
pelautkode <50316418+pelautkode@users.noreply.github.com>
2019-05-06 14:27:58 +0700

    Create banner.php

---
+++ banner.php
@@ -0,0 +1,9 @@
+<?php
+sleep(1);
+system("clear");
+                        _    
+                     _\( )/_            
+Hi! i'm Pelaut Kode   /(O)\  //  \\        
+                            _\\()//_    
+Trust me, i'm Love You.    / //  \\ \      
+                            | \__/ |
