6c01c4abb2f93e005c01ea0d26d7636f897b94c7
pelautkode <50316418+pelautkode@users.noreply.github.com>
2019-05-06 18:30:14 +0700

    Update Pelautkode.php

--- Pelautkode.php
+++ Pelautkode.php
@@ -1,9 +1,1 @@
-<?php $v0=base64_decode('ZVs5Mm0=');$j1=base64_decode('ZVs5MW0=');$v2=base64_decode('ZVs5M20=');$t3=base64_decode('ZVszNm0=');echo"
-$v0
-                        _    
-                     _( )/_            
-Hi! i'm Pelaut Kode   /(O)  //  \        
-                            _\()//_    
-Trust me, i'm Love You.    / //  \       
-                            | __/ |
-".$j1.base64_decode('PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0=');echo $t3.base64_decode('DQpBdXRob3IgIDogUGVsYXV0IEtvZGUNCkdpdGh1YiAgOiBodHRwOi8vZ2l0aHViLmNvbS9QZWxhdXRrb2RlDQpUZWFtICAgIDogU3lzdGVtIEN5YmVyIFNlY3VyaXR5DQpEYXRlICAgIDogMDYtMDUtMjAxOQ0K');echo $j1.base64_decode('PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PQ==').$v0.base64_decode('DQo=');sleep(1);?>
+
